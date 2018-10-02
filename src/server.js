60c891d3e83599bf2ced04861c309dd336d12ec1
Gbolahan Olawuyi <gbolahanolawuyi4u@gmail.com>
2018-10-02 16:21:32 +0100

    mongoose db connection

--- src/server.js
+++ src/server.js
@@ -1,6 +1,14 @@
 import express from 'express'
 import http from 'http'
 import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+mongoose.connect('mongodb://localhost:27017/db_name_goes_here')
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', () => {
+  console.log("we're connected!");
+})
 
 import Api from '../routes/index'
 
